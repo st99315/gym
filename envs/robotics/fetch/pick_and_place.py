6c3c87df584134e9dc548a01bb76be8a596d9e47
youjun <st99315@yahoo.com.tw>
2018-05-14 02:57:44 +0800

    Revise Fetch Env

---
+++ envs/robotics/fetch/pick_and_place.py
@@ -0,0 +1,33 @@
+from gym import utils
+from gym.envs.robotics import fetch_env
+
+
+class FetchPickAndPlaceEnv(fetch_env.FetchEnv, utils.EzPickle):
+    def __init__(self, reward_type='sparse', xml_file='fetch/pick_and_place.xml'):
+        initial_qpos = {
+            'robot0:slide0': 0.405,
+            'robot0:slide1': 0.48,
+            'robot0:slide2': 0.0,
+            'object0:joint': [1.25, 0.53, 0.4, 1., 0., 0., 0.],
+        }
+        fetch_env.FetchEnv.__init__(
+            self, xml_file, has_object=True, block_gripper=False, n_substeps=20,
+            gripper_extra_height=0.2, target_in_the_air=True, target_offset=0.0,
+            obj_range=0.15, target_range=0.15, distance_threshold=0.02,
+            initial_qpos=initial_qpos, reward_type=reward_type)
+        utils.EzPickle.__init__(self)
+
+class FetchPickAndPlaceJointEnv(fetch_env.FetchJointEnv, utils.EzPickle):
+    def __init__(self, reward_type='sparse', xml_file='fetch/pick_and_place.xml'):
+        initial_qpos = {
+            'robot0:slide0': 0.405,
+            'robot0:slide1': 0.48,
+            'robot0:slide2': 0.0,
+            'object0:joint': [1.25, 0.53, 0.4, 1., 0., 0., 0.],
+        }
+        fetch_env.FetchJointEnv.__init__(
+            self, xml_file, has_object=True, block_gripper=False, n_substeps=20,
+            gripper_extra_height=0.2, target_in_the_air=True, target_offset=0.0,
+            obj_range=0.15, target_range=0.15, distance_threshold=0.02,
+            initial_qpos=initial_qpos, reward_type=reward_type)
+        utils.EzPickle.__init__(self)
